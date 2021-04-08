2c94c451601fca2ed1a60ad2ce577b887414a1e0
Paweł Goliszewski <pawlik9.hamster@gmail.com>
2021-04-08 23:25:45 +0200

    improvenment

--- zero_shot.py
+++ zero_shot.py
@@ -1,40 +1,16 @@
 import torch
 import clip
-from data import PokemonDataset
 from tqdm import tqdm
-
+from data import PokemonDataset
 from collections import defaultdict
-
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("ViT-B/32", device=device)
-optimizer = torch.optim.SGD(model.parameters(), lr=0.0005, momentum=0.5)
 
+print(device)
 dataset = PokemonDataset()
 classes = list(dataset.get_classes())
-text = clip.tokenize(classes).to(device)
-examples = dataset.fetch_per_type_examples()
 
-for _ in tqdm(range(5)):
-    optimizer.zero_grad()
-
-    labels = []
-    images = []
-    for ex in examples:
-        image, label = ex
-        labels.append(classes.index(label))
-        image_tensor = preprocess(image).unsqueeze(0).to(device)
-        images.append(image_tensor)
-    
-    logits, _ = model(torch.cat(images), text)
-    probs = logits.softmax(dim=-1)
-
-    loss = torch.nn.functional.cross_entropy(probs, torch.as_tensor(labels).to(device))
-    print(loss)
-    loss.backward()
-    optimizer.step()
-
-# evaluation
 proper_one = defaultdict(int)
 proper_two = defaultdict(int)
 all_one = defaultdict(int)
@@ -44,8 +20,6 @@ for image, real_labels in tqdm(dataset):
     text = clip.tokenize(classes).to(device)
 
     with torch.no_grad():
-        image_features = model.encode_image(image_tensor)
-        text_features = model.encode_text(text)
 
         logits_per_image, logits_per_text = model(image_tensor, text)
         probs = logits_per_image.softmax(dim=-1).cpu().numpy()
@@ -74,4 +48,3 @@ print(f"All pokemon accuracy: {accuracy_all}")
 accuracy_per_class = {x:(proper_one[x] + proper_two[x])/(all_one[x] + all_two[x]) for x in classes}
 
 print(accuracy_per_class)
-
