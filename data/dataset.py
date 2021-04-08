12ca0c76721967b4b47f7ef2c07f6a632aa86101
konradwojtowicz <wooyu69@gmail.com>
2021-04-08 07:42:25 +0200

    fewshot wip

--- data/dataset.py
+++ data/dataset.py
@@ -4,10 +4,11 @@ import pandas as pd
 from torch.utils.data import Dataset
 from PIL import Image
 
+
 def prep():
     pokemons = pd.read_csv('./data/pokedex.csv')
     numbers = []
-    for i in range(1, len(pokemons)+1):
+    for i in range(1, len(pokemons) + 1):
         numbers.append(i)
     pokemons['pkn'] = numbers
     img_dir = './data/images'
@@ -30,8 +31,10 @@ def prep():
     result = pokemons.merge(dataframe_img, left_on='pkn', right_on='pkn')
     return result
 
+
 class PokemonDataset(Dataset):
     def __init__(self, transform=None):
+        super().__init__()
         self.img_labels = prep()
         self.transform = transform
 
@@ -47,8 +50,16 @@ class PokemonDataset(Dataset):
         label2 = pokemon["Type2"]
         label2 = None if pd.isnull(label2) else label2
         return image, [label1, label2]
-    
+
     def get_classes(self):
         type1 = pd.unique(self.img_labels["Type1"])
         type2 = [x for x in pd.unique(self.img_labels["Type2"]) if not pd.isnull(x)]
         return set(type1) | set(type2)
+
+    # to be used for few-shot learning regime; return n examples of each type
+    def fetch_per_type_examples(self, n_examples=1):
+        examples = pd.concat([
+                self.img_labels.query(f'"{cls}" in Type1 or "{cls}" in Type2').sample(n_examples)
+                for cls in self.get_classes()
+        ])
+        return examples
